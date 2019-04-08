204bb57756307b19bbcd283565d0c2537d3a43aa
Raul Israel Garcia Barajas <44442234+ds365@users.noreply.github.com>
2019-04-07 23:19:25 -0500

    Delete Practica2.m

--- Practicas/Practica 1/Practica 2/Practica2.m
+++
@@ -1,8 +0,0 @@
-syms q1
-syms q2
-syms l1
-syms q3
-a=[cos(q1) 0 sin(q1) 0;sin(q1) 0 cos(q1) 0;0 1 0 l1;0 0 0 1];
-b=[cos(q2) 0 -sin(q2) 0;sin(q2) 0 cos(q2) 0;0 -1 0 0;0 0 0 1];
-c=[1 0 0 0;0 1 0 0;0 0 1 q3;0 0 0 1];
-d=c*b*a
